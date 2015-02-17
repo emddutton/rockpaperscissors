b53b3668301efa04147a53e021c67000c254ee64
Elizabeth Dutton <emddutton@gmail.com>
2015-02-16 23:07:43 -0700

    check console log for win

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -2,7 +2,6 @@
  * Created by emddutton on 2/5/2015.
  */
 $(document).ready(function() {
-    var duration = 250;
     var shake = $(".handshake");
     var counter = $(".choose h2");
     var choices = ["rock", "paper", "scissors"];
@@ -12,13 +11,8 @@ $(document).ready(function() {
 
         shake.addClass('shake');
 
-           function countdown() {
-                counter.remove();
+        counter.remove();
 
-            }
-
-        countdown(duration);
-        var thisClick = this;
         compChoice = choices[Math.floor(Math.random()*3)];
         userChoice = this.id;
         console.log(compChoice);
@@ -26,7 +20,18 @@ $(document).ready(function() {
                 shake.hide();
                 $(".computer > .left." + compChoice).show();
                 $(".you > .right." + userChoice).show();
-                
+
+                if(compChoice === userChoice){
+
+                    console.log("tie");
+                }
+                else if((userChoice == 'rock' && compChoice == 'scissors')|| (userChoice == 'scissors' && compChoice == 'paper') || (userChoice == 'paper' && compChoice == 'rock')){
+                    console.log("winner winner chicken dinner");
+                }
+                else if((userChoice == 'rock' && compChoice == 'paper') || (userChoice == 'scissors' && compChoice == 'rock') || (userChoice == 'paper' && compChoice == 'scissors') ){
+                    console.log("loser");
+                }
+
             }, 1000);
 
 
