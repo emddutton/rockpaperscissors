53861f12e43800d28092c117d62de2e47b5d394b
Elizabeth Dutton <emddutton@gmail.com>
2015-02-17 15:58:23 -0700

    winner announced

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 
         shake.addClass('shake');
 
-        counter.remove();
+        counter.hide();
 
         compChoice = choices[Math.floor(Math.random()*3)];
         userChoice = this.id;
@@ -22,13 +22,15 @@ $(document).ready(function() {
                 $(".you > .right." + userChoice).show();
 
                 if(compChoice === userChoice){
-
+                    counter.show().text("It's a tie!");
                     console.log("tie");
                 }
                 else if((userChoice == 'rock' && compChoice == 'scissors')|| (userChoice == 'scissors' && compChoice == 'paper') || (userChoice == 'paper' && compChoice == 'rock')){
+                    counter.show().text("You win!");
                     console.log("winner winner chicken dinner");
                 }
                 else if((userChoice == 'rock' && compChoice == 'paper') || (userChoice == 'scissors' && compChoice == 'rock') || (userChoice == 'paper' && compChoice == 'scissors') ){
+                    counter.show().text("You lose.");
                     console.log("loser");
                 }
 
